f18e3f6388bcdb03ed1b127359ca77b051d489d4
viliussapauskas <vilius.sapauskas.1997@gmail.com>
2022-06-07 23:21:28 +0300

    Minor updates

--- src/react/components/spinner/spinner.tsx
+++ src/react/components/spinner/spinner.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react'
 import { ActivityIndicator } from 'react-native-paper'
 
-export const Spinner: FC = () => {
-    return (
-        <ActivityIndicator testID="spinner" size={'large'} color={'#f4511e'} />
-    )
-}
+export const Spinner: FC = () => (
+    <ActivityIndicator testID="spinner" size={'large'} color={'#f4511e'} />
+)
